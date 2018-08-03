fd996b7c2d6e5799b40850a47e78c6aa86a134ba
longll <long3l@qq.com>
2018-08-04 00:02:52 +0800

    quick sort

--- program/src/com/sysu/Main.java
+++ program/src/com/sysu/Main.java
@@ -1,8 +1,13 @@
 package com.sysu;
 
+import com.sysu.Sort;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] arr = {1, 3, 5, 6, 14};
+        Sort sort = new Sort();
+        sort.quickSortWithRecursion(arr, 0, arr.length-1);
+        System.out.println(arr);
     }
 }
---
+++ program/src/com/sysu/Sort.java
@@ -0,0 +1,81 @@
+package com.sysu;
+
+import java.util.Stack;
+
+public class Sort {
+
+    public void quickSortWithRecursion(int[] arr, int left, int right) {
+        if (arr.length == 1 || arr.length == 0 || left >= right) {
+            return;
+        }
+        int i = left, j = right;
+        int pivot = arr[left];
+
+        while (i < j) {
+            while (i < j && arr[j] >= pivot) {
+                j--;
+            }
+            if (i < j) {
+                arr[i] = arr[j];
+            }
+
+            while (i < j && arr[i] <= pivot) {
+                i++;
+            }
+            if (i < j) {
+                arr[j] = arr[i];
+            }
+        }
+        arr[i] = pivot;
+
+        quickSortWithRecursion(arr, left, i-1);
+        quickSortWithRecursion(arr, i+1, right);
+    }
+
+    public void quickSortnotRecursion(int[] arr) {
+        if (arr.length <= 1) {
+            return;
+        }
+        Stack<Integer> stack  = new Stack<>();
+
+        stack.push(0); // start
+        stack.push(arr.length-1); // end
+
+        while (!stack.empty()) {
+            int j = stack.pop(); //end
+            int i = stack.pop(); // start
+            int pivotIndex = partition(arr, i, j);
+            if (pivotIndex > i) {
+                stack.push(i);
+                stack.push(pivotIndex-1);
+            }
+            if (pivotIndex < j) {
+                stack.push(pivotIndex+1);
+                stack.push(j);
+            }
+        }
+    }
+
+    public int partition(int[] arr, int left, int right) {
+        int i = left, j = right;
+        int pivot = arr[left];
+
+        while (i < j) {
+            while (i < j && arr[j] >= pivot) {
+                j--;
+            }
+            if (i < j) {
+                arr[i] = arr[j];
+            }
+
+            while (i < j && arr[i] <= pivot) {
+                i++;
+            }
+            if (i < j) {
+                arr[j] = arr[i];
+            }
+        }
+        arr[i] = pivot;
+        return i;
+    }
+}
