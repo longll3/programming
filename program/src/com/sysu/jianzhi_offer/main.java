2684bc91eaf54b056797cfbe00e895b7f9801045
longll <long3l@qq.com>
2018-08-15 23:34:59 +0800

    打卡 8/15

--- program/src/com/sysu/jianzhi_offer/main.java
+++ program/src/com/sysu/jianzhi_offer/main.java
@@ -10,7 +10,10 @@ public class main {
 
 
 
-		ArrayList<ArrayList<Integer>> a = FindContinuousSequence(3);
+		int[] num = {0,3,2,6,4};
+		selectionSort(num);
+		System.out.println(isContinuous(num));
+
 
 
     }
@@ -122,7 +125,6 @@ public class main {
 			swap(str,i, begin); // 要复位，不然上一步的交换会使原始字符串发生变化，那么在分割的时候就会漏掉很多情况
 		}
 
-		return;
 
 	}
 
@@ -291,5 +293,50 @@ public class main {
 		return res;
 	}
 
+
+	/**
+	 * @brief LL今天心情特别好,因为他去买了一副扑克牌,发现里面居然有2个大王,2个小王(一副牌原本是54张^_^)...
+	 * 他随机从中抽出了5张牌,想测测自己的手气,看看能不能抽到顺子,如果抽到的话,他决定去买体育彩票,嘿嘿！！
+	 * “红心A,黑桃3,小王,大王,方片5”,“Oh My God!”不是顺子.....
+	 * LL不高兴了,他想了想,决定大\小 王可以看成任何数字,并且A看作1,J为11,Q为12,K为13。
+	 * 上面的5张牌就可以变成“1,2,3,4,5”(大小王分别看作2和4),“So Lucky!”。
+	 * LL决定去买体育彩票啦。
+	 * 现在,要求你使用这幅牌模拟上面的过程,然后告诉我们LL的运气如何， 如果牌能组成顺子就输出true，否则就输出false。
+	 * 为了方便起见,你可以认为大小王是0。
+	 *
+	 * 1. 除0外没有重复的数
+	 * 2. max - min < 5
+	 */
+	public static boolean isContinuous(int [] numbers) {
+		if (numbers.length == 0 || numbers == null) return false;
+
+		selectionSort(numbers);
+
+		int min = numbers[numbers.length-1];
+		for (int i = 0; i < numbers.length-1; i++) {
+			if (numbers[i] == 0) continue;
+			if (numbers[i] == numbers[i + 1]) return false;
+			if (numbers[i] < min) min = numbers[i];
+		}
+		if (numbers[numbers.length-1] - min >= 5) return false;
+
+		return true;
+	}
+
+	public static void selectionSort(int[] numbers) {
+		for (int i = numbers.length-1; i > 1; i--) {
+			int max_index = 0;
+			int max = 0;
+			for (int j = 0; j <= i; j++) {
+				if (max < numbers[j]) {
+					max = numbers[j];
+					max_index = j;
+				}
+			}
+			int tmp = numbers[max_index];
+			numbers[max_index] = numbers[i];
+			numbers[i] = tmp;
+		}
+	}
 
 }
