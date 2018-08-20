c2aab3f24f6060af0134d13c916561a783afc0ca
longll <long3l@qq.com>
2018-08-20 22:25:11 +0800

    8/20 打卡

--- program/src/com/sysu/jianzhi_offer/TreeNode.java
+++ program/src/com/sysu/jianzhi_offer/TreeNode.java
@@ -1,5 +1,6 @@
 package com.sysu.jianzhi_offer;
 
+import java.net.ConnectException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -92,6 +93,26 @@ public class TreeNode {
 
         return res;
     }
+
+    /**
+     * 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。要求不能创建任何新的结点，只能调整树中结点指针的指向。
+     */
+//    public TreeNode Convert(TreeNode pRootOfTree) {
+//
+//
+//
+//
+//    }
+
+//    public TreeNode Link(TreeNode node) {
+//        if (node == null) return null;
+//
+//        TreeNode left = Link(node.left);
+//        left.left = node;
+//        node.right = Link(node.right);
+//
+//        return
+//    }
 
     /**
      * 给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。
