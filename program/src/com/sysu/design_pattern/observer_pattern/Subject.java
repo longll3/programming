0cc5e8f9e9fab207520e8066bf69c7754192979f
longll <long3l@qq.com>
2018-10-30 14:11:00 +0800

    观察者模式增加注解

--- program/src/com/sysu/design_pattern/observer_pattern/Subject.java
+++ program/src/com/sysu/design_pattern/observer_pattern/Subject.java
@@ -2,7 +2,9 @@ package com.sysu.design_pattern.observer_pattern;
 
 /**
  * 观察者模式 定义了对象之间的一对多依赖，这样一来，当一个对象改变状态时，它的所有依赖者都会收到通知并更新。
+ * Java jdk有自己实现观察者模式 java.util.Observable 和 java.util.Observer，
  */
+
 
 /**
  * 观察者模式中的主题，负责状态发生改变时通知所有订阅了该主题的观察者（notigyObservers）
